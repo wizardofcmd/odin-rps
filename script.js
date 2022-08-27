816be10a53c26600bbbe285d1888da4e7ffed399
Sorin <sorinnechiforora@gmail.com>
2022-08-27 10:51:53 +0100

    Refactor const HANDSIGNS to handSigns

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const HANDSIGNS = ["Rock", "Paper", "Scissors"]
+const handSigns = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
     // Generates an int number between 1, 2 or 3
-    let index = Math.floor(Math.random() * HANDSIGNS.length);
+    let index = Math.floor(Math.random() * handSigns.length);
     // Passes in the chosen int as the index, selecting a handsign
-    return HANDSIGNS[index]
+    return handSigns[index]
 }
