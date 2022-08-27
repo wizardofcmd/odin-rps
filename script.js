3876086c754170c2bb77cb14778993b7764a02bb
Sorin <sorinnechiforora@gmail.com>
2022-08-27 11:52:05 +0100

    Add rock paper scissors calculation

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const handSigns = ["Rock", "Paper", "Scissors"];
+const handSigns = ["ROCK", "PAPER", "SCISSORS"];
+const draw = "It's a draw! Computer also chose "
+const win = "You win! Computer chose "
+const lose = "You lose! Computer chose "
+const errorHandsign = "Error, incorrect handsign chosen.";
+const introMsg = "Welcome to Rock Paper Scissors. Choose a handsign."
+let playerAnswer;
 
 function getComputerChoice() {
     // Generates an int number between 1, 2 or 3
@@ -6,3 +12,41 @@ function getComputerChoice() {
     // Passes in the chosen int as the index, selecting a handsign
     return handSigns[index]
 }
+
+function playRockPaperScissors(playerSelection, computerSelection = getComputerChoice()) {
+    playerSelection = playerSelection.toUpperCase();
+    switch (playerSelection) {
+        case "ROCK":
+            switch (computerSelection) {
+                case "ROCK":
+                    return draw + computerSelection;
+                case "PAPER":
+                    return lose + computerSelection;
+                case "SCISSORS":
+                    return win + computerSelection;
+            }
+        case "PAPER":
+            switch (computerSelection) {
+                case "ROCK":
+                    return win + computerSelection;
+                case "PAPER":
+                    return draw + computerSelection;
+                case "SCISSORS":
+                    return lose + computerSelection;
+            }        
+        case "SCISSORS":
+            switch (computerSelection) {
+                case "ROCK":
+                    return lose + computerSelection;
+                case "PAPER":
+                    return win + computerSelection;
+                case "SCISSORS":
+                    return draw + computerSelection;
+            }
+        default:
+            return errorHandsign;
+    }
+}
+
+playerAnswer = prompt(introMsg);
+console.log(playRockPaperScissors(playerAnswer));
