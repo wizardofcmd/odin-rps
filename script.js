05e3ba39c0f9bb248ce97d66f5043dc33cfca478
Sorin <sorinnechiforora@gmail.com>
2022-09-10 20:02:19 +0100

    Add score calculation after player choice

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const handSigns = ["ROCK", "PAPER", "SCISSORS"];
-const draw = "It's a draw! Computer also chose "
-const win = "You win! Computer chose "
-const lose = "You lose! Computer chose "
-const introMsg = "Welcome to Rock Paper Scissors. Choose a handsign."
-const buttons = document.querySelectorAll('button');
+const draw = "It's a draw! Computer also chose ";
+const win = "You win! Computer chose ";
+const lose = "You lose! Computer chose ";
+const introMsg = "Welcome to Rock Paper Scissors. Choose a handsign.";
+const buttons = document.querySelectorAll("button");
+const output = document.querySelector(".output");
+const computerChoiceElem = document.querySelector("#computer-choice");
+const playerScoreCell = document.querySelector("#player-score-cell");
+const computerScoreCell = document.querySelector("#computer-score-cell");
+let playerScore = 0;
+let compScore = 0;
 let playerChoice;
+let result;
 
 function getComputerChoice() {
     // Generates an int number between 1, 2 or 3
     let index = Math.floor(Math.random() * handSigns.length);
     // Passes in the chosen int as the index, selecting a handsign
-    return handSigns[index]
+    return handSigns[index];
 }
 
-function playRockPaperScissors(playerSelection,
-    computerSelection = getComputerChoice()) {
+function playRockPaperScissors(
+    playerSelection, computerSelection = getComputerChoice()) {
+    computerChoiceElem.textContent = computerSelection;
     switch (playerSelection) {
         case "ROCK":
             switch (computerSelection) {
@@ -46,20 +54,42 @@ function playRockPaperScissors(playerSelection,
     }
 }
 
-function game() {
+function updateScore(roundResult) {
+    if (roundResult.includes(win)) {
+        ++playerScore;
+        playerScoreCell.textContent = playerScore;
+    }
+    else if (roundResult.includes(lose)) {
+        ++compScore;
+        computerScoreCell.textContent = compScore;
+    }
+}
 
+function game() {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             playerChoice = button.textContent.toUpperCase();
-            console.log(playerChoice)
-
             result = playRockPaperScissors(playerChoice);
-            console.log(result);
+            updateScore(result);
+
+            if (playerScore === 5) {
+                const winElement = document.createElement('p');
+                winElement.classList.add('win-txt');
+                winElement.textContent = 
+                    "Victory! Man triumphs against machine.";
+                output.appendChild(winElement);
+                return;
+            }
+            else if (compScore === 5) {
+                const loseElement = document.createElement('p');
+                loseElement.classList.add('lose-txt');
+                loseElement.textContent = 
+                    "Failure! Machines reign supreme.";
+                output.appendChild(loseElement);
+                return;
+            }
         });
     });
-
-    let playerScore = 0;
-    let compScore = 0;
 }
 
 game();
